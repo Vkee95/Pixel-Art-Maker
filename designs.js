4f1774594a43757f3e45468942c0e17c28185b0c
scampcoder <63260779+scampcoder@users.noreply.github.com>
2020-04-27 08:29:42 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,14 +1,16 @@
 // Select color input
 let color = document.getElementById('colorPicker');
-// Select size input
+// Access to sizing form element
 let sizing = document.getElementById('sizePicker')
+//Access to design canvas element
 let canvas = document.getElementById('pixelCanvas')
+//Select size input values
 let height =  document.getElementById('inputHeight').value;
 let width = document.getElementById('inputWidth').value;
-
+//Access to submit button
 let submit = document.querySelector('input[type="submit"]')
-// When size is submitted by the user, call makeGrid()
 
+// When size is submitted by the user, call makeGrid()
 sizing.addEventListener('submit', function(event){
   let height = document.getElementById('inputHeight').value;
   let width = document.getElementById('inputWidth').value;
@@ -19,6 +21,7 @@ sizing.addEventListener('submit', function(event){
 });
 
 function makeGrid(height, width) {
+  canvas.innerHTML = null;
   //make grid rows
   for(r = 0; r < height; r++){
     let row = canvas.insertRow(r);
