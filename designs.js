3529ed21424ff2609a18dd34ac44605a1a16323f
scampcoder <63260779+scampcoder@users.noreply.github.com>
2020-04-26 21:38:56 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -13,21 +13,22 @@ sizing.addEventListener('submit', function(event){
   let height = document.getElementById('inputHeight').value;
   let width = document.getElementById('inputWidth').value;
   let canvas = document.getElementById('pixelCanvas');
+  //prevent default of form submit
   event.preventDefault();
   makeGrid(height, width);
 });
 
 function makeGrid(height, width) {
+  //make grid rows
   for(r = 0; r < height; r++){
     let row = canvas.insertRow(r);
+    //make grid cells w/i rows
     for(c = 0; c < width; c++){
       let cell = row.insertCell(c);
+      //when cell is clicked, change to selected color
       cell.addEventListener('click', function(event){
         cell.style.backgroundColor = color.value
       })
     }
   }
 };
-
-//Things to Fix Today!
-//Delete old canvas if new sizing values supplied
